dbbca471be99a8791f99f08297e35677264539c8
Ellen Wong <ellen.l.wong@gmail.com>
2016-10-11 21:52:34 -0700

    more code added

--- build.sbt
+++ build.sbt
@@ -5,8 +5,8 @@ version := "1.0"
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "1.5.2",
-  "org.apache.spark" %% "spark-mllib" % "1.5.2"
+  "org.apache.spark" %% "spark-core" % "1.6.0",
+  "org.apache.spark" %% "spark-mllib" % "1.6.0"
 )
 
     
--- src/main/scala/ForestCoverTypePredictionApp.scala
+++ src/main/scala/ForestCoverTypePredictionApp.scala
@@ -5,6 +5,13 @@ import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.mllib.tree.{RandomForest, DecisionTree}
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.util.MLUtils
 
 object ForestCoverTypePredictionApp {
   def main (args: Array[String]){
@@ -14,16 +21,14 @@ object ForestCoverTypePredictionApp {
   }
 }
 
-//TODO: add log4j to remove sparkJunk
-
 class ForestCoverTypePredictionApp {
-  // TODO: put CSV into hdfs
+
   val currentDir = new File(".").getAbsolutePath
   Helper.printlnLoudly(currentDir)
   val inputData = s"$currentDir/src/main/resources/covtype.csv"
   //Read the raw file
   val conf = new SparkConf().setAppName("ForestCoverTypePredictionApp").setMaster("local")
-  val sc = new SparkContext(conf)
+  val numOfForestType = 7;
 
   // feature extraction into labelPoint
   // split data training/ cross validation/ test (80/10/10)
@@ -31,6 +36,7 @@ class ForestCoverTypePredictionApp {
   // Tuning decision tree [impurity, depth, bias]
   // display prediction results
   def run(): Boolean = {
+    val sc = new SparkContext(conf)
     val rawForestData: RDD[String] = sc.textFile(inputData)
     Helper.printlnLoudly(rawForestData.count())
 
@@ -38,23 +44,36 @@ class ForestCoverTypePredictionApp {
       singleDataPoint =>
         lazy val dataArray = singleDataPoint.split(",").map(_.toDouble)
         val featureVector = dataArray.take(singleDataPoint.length - 1)
-        val label = dataArray.last
+        val label = dataArray.last -1
         LabeledPoint(label, Vectors.parse(featureVector.toVector.toString()))
     }
 
-    val (trainingData, crossValidationData, testData) = data.randomSplit(Array(80, 10, 10))
-    val asdf = testData //???
+    val Array(trainingData, crossValidationData, testData) = data.randomSplit(Array(80, 10, 10))
+    trainingData.cache()
+    crossValidationData.cache()
+    testData.cache()
 
 
-    /*case class LabeledPoint @Since("1.0.0") (
-    @Since("0.8.0") label: Double,
-    @Since("1.0.0") features: Vector) {
-  override def toString: String = {
-    s"($label,$features)"
-  }
-*/
+    val numClasses = numOfForestType // because label value needs to be < numClass, all label values subtracted 1
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val impurity = "gini" // "entropy"
+    val maxDepth = 5
+    val maxBins = 32
 
+    // create a model from training data
+    val decisionTreeModel = DecisionTree.trainClassifier(
+      input = trainingData,
+      numClasses = numClasses,
+      categoricalFeaturesInfo = categoricalFeaturesInfo,
+      impurity = impurity,
+      maxDepth = maxDepth,
+      maxBins = maxBins
+    ) // what is the numTree param??
 
+    // get some prediction value from cross validation data
+    // calculate auc/ accuracy and precision
+
+    //
 
 
     sc.stop()
@@ -62,14 +81,23 @@ class ForestCoverTypePredictionApp {
   }
 }
 
-class ForestData{
-  def ForestData
-
-}
+//Forest Data Fields
+//Id,Elevation,Aspect,Slope,Horizontal_Distance_To_Hydrology,Vertical_Distance_To_Hydrology,
+// Horizontal_Distance_To_Roadways,Hillshade_9am,Hillshade_Noon,Hillshade_3pm,
+// Horizontal_Distance_To_Fire_Points,Wilderness_Area1,Wilderness_Area2,Wilderness_Area3,Wilderness_Area4,
+// Soil_Type1,Soil_Type2,Soil_Type3,Soil_Type4,Soil_Type5,Soil_Type6,Soil_Type7,Soil_Type8,Soil_Type9,Soil_Type10,
+// Soil_Type11,Soil_Type12,Soil_Type13,Soil_Type14,Soil_Type15,Soil_Type16,Soil_Type17,Soil_Type18,Soil_Type19,
+// Soil_Type20,Soil_Type21,Soil_Type22,Soil_Type23,Soil_Type24,Soil_Type25,Soil_Type26,Soil_Type27,Soil_Type28,
+// Soil_Type29,Soil_Type30,Soil_Type31,Soil_Type32,Soil_Type33,Soil_Type34,Soil_Type35,Soil_Type36,Soil_Type37,
+// Soil_Type38,Soil_Type39,Soil_Type40,Cover_Type
 
 object Helper {
   def printlnLoudly(str: Any) = {
-    println(s"############### $str")
+    println(s"\n############### $str\n")
   }
 }
 
+//TODO: add a step actually parse data into a case class, for better logging
+// TODO: put CSV into hdfs
+//TODO: add log4j to remove sparkJunk
+//TODO: how to implement one hot encoding from rawdata?
